056d072af44d195e16953705a240ef8ae6d0afc5
SteveLorde <mostafa.maher98@gmail.com>
2025-09-30 22:20:30 +0300

    Improved logger service

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -3,11 +3,11 @@ const logger = console;
 export function log(message: string) {
   const time = new Date().toLocaleTimeString();
 
-  logger.log(time + " " + message);
+  logger.log(`${time} ${message}`);
 }
 
 export function error(message: string) {
   const time = new Date().toLocaleTimeString();
 
-  logger.error(time + " " + message);
+  logger.error(`${time} ${message}`);
 }
